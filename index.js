a3b462c941ee9735651f2522c315860fc8a229c6
ericaaaa2 <163474434+ericaaaa2@users.noreply.github.com>
2024-09-03 22:57:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const primeiroNome = "Erica";
+const sobrenome = "Celestino de lara";
+const idade = 19;
+const cidadeNatal = "São Soje dos Pinhais";
+const altura = 1.65;
+
+
+
+console.log("O meu primeiro nome é");
+console.log(primeiroNome);
+console.log("Meu sobrenome é");
+console.log(sobrenome);
+console.log("Minha idade");
+console.log(idade);
+console.log("Minha cidade natal");
+console.log(cidadeNatal);
+console.log("Minha altura");
+console.log(altura);
